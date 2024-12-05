c1023de3130467a6b2c46cd670266b080c1a6e0c
guilhermemaia2005 <guilhermemaia0505@gmail.com>
2024-12-05 11:26:00 -0300

    Add files via upload

---
+++ Aula 2/Aula2.py
@@ -0,0 +1,17 @@
+print('Bom dia!')
+
+nome_usuario = 'Guilherme Fernandes' # tipo -> str = string
+
+idade_usuario = 19 # tipo -> int = integer
+
+altura_usuario = 1.71 # tipo -> float = flutuantes
+
+maior_idade = True # tipo -> bool = boolean
+
+print('Seja Bem-Vindo,',nome_usuario)
+
+print('Sua idade é',idade_usuario,'anos') 
+
+print('Você tem',altura_usuario,'metros.')
+
+print('Você é maior de idade?',maior_idade)
